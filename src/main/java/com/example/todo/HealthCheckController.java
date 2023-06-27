c6b763fe96737f80ee152a2c6ae5dafa545d10b1
Bcacao <126742424+Bcacao@users.noreply.github.com>
2023-06-27 10:40:39 +0900

    v0.0.2

--- src/main/java/com/example/todo/HealthCheckController.java
+++ src/main/java/com/example/todo/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.example.todo;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HealthCheckController {
 
-
     @GetMapping("/")
-    public ResponseEntity<String> healthCheck(){
-        log.info("server is running ..... i'm healthy!");
-        return ResponseEntity.ok().body("it's ok!");
+    public ResponseEntity<?> healthCheck() {
+        log.info("server is running.... I'm healthy!");
+        return ResponseEntity.ok()
+                .body("It's OK!");
     }
-
 }
